9fb1cf21b4edfcc01ab523917c300bdd05f5e850
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2020-02-07 18:24:55 +0000

    Add files via upload

---
+++ Year 2/DBMS/Coursework 2/hk42.sql
@@ -0,0 +1,67 @@
+#F28DM CW2
+
+
+
+#username = hk42
+
+
+
+#question 1
+
+SELECT….
+
+
+
+#question 2
+
+
+
+#question 3
+
+
+
+#question 4
+
+
+
+#question 5
+
+
+
+#question 6
+
+
+
+#question 7
+
+
+
+#question 8
+
+
+
+#question 9
+
+
+
+#question 10
+
+
+
+#question 11
+
+
+
+#question 12
+
+
+
+#question 13
+
+
+
+#question 14
+
+
+
+#question 15
