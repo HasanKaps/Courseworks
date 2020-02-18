7d13cc69c1b478ffb24ba65e97ce2bfef446c935
Kapadia <hk42@hw.ac.uk>
2020-02-18 23:52:35 +0000

    updated sql file

--- Year 2/DBMS/Coursework 2/hk42.sql
+++ Year 2/DBMS/Coursework 2/hk42.sql
@@ -14,20 +14,17 @@ SELECT COUNT(sex) FROM imdb_actors WHERE sex ="F";
 
 #question 2
 
-SELECT MIN(year), title FROM imdb_movies;
-
+SELECT year , title FROM imdb_movies ORDER BY year ASC LIMIT 1;
 
 
 #question 3
  
-SELECT COUNT(*) FROM(SELECT COUNT(*) FROM imdb_movies2directors GROUP BY movieid HAVING count(*) > 5)a;
-
-
+SELECT COUNT(*) FROM(SELECT COUNT(*), movieid FROM imdb_movies2directors GROUP BY movieid HAVING count(*) > 5)a;
 
 #question 4
 
-SELECT title FROM imdb_movies, (SELECT MAX(count),movieid as id FROM(SELECT COUNT(*) AS count, movieid FROM imdb_movies2directors GROUP BY movieid)a)b WHERE movieid = id;
-
+SELECT imdb_movies.movieid , title, COUNT(*) AS TitleCount FROM imdb_movies2directors JOIN imdb_movies ON imdb_movies2directors.movieid = imdb_movies.movieid
+GROUP BY movieid ORDER BY TitleCount DESC LIMIT 1;
 
 
 #question 5
@@ -35,147 +32,73 @@ SELECT title FROM imdb_movies, (SELECT MAX(count),movieid as id FROM(SELECT COUN
 SELECT SUM(time1) FROM imdb_runningtimes,(SELECT movieid as id FROM imdb_movies2directors WHERE genre='Sci-Fi')a WHERE movieid = id;
 
 
-
 #question 6
 
-SELECT COUNT(*) FROM imdb_movies2actors WHERE actorid IN (SELECT actorid FROM imdb_actors WHERE name = 'McGregor, Ewan' OR name LIKE 'Carlyle, Robert (I)');
+SELECT COUNT(*) FROM(SELECT movieid, name, COUNT(movieid) as MovieCount FROM imdb_movies2actors JOIN imdb_actors ON imdb_movies2actors.actorid = imdb_actors.actorid
+WHERE name = 'McGregor, Ewan' OR name LIKE 'Carlyle, Robert (I)' GROUP BY movieid HAVING MovieCount > 1)A;
 
 
 
 #question 7
 
-SELECT COUNT(*) FROM
-(
-SELECT act1, act2, Count(*) as count
-FROM
-(
-SELECT movie1, act1, movie2 ,act2 FROM 
-(
-SELECT actorid as act1, movieid as movie1 FROM(SELECT actorid as id,COUNT(*) as count FROM imdb_movies2actors GROUP BY actorid)a JOIN imdb_movies2actors ON id = actorid  WHERE count >= 10
-)a
-JOIN
-(
-SELECT actorid as act2, movieid as movie2 FROM(SELECT actorid as id,COUNT(*) as count FROM imdb_movies2actors GROUP BY actorid)a JOIN imdb_movies2actors ON id = actorid  WHERE count >= 10
-)b
-ON movie1 = movie2 AND act1 != act2 
-)c GROUP BY act1, act2 HAVING count >= 10
-)d;
-
+SELECT COUNT(DISTINCT(act1)) AS ActorsCount FROM (SELECT act1, act2, Count(*) as count FROM (SELECT movie1, act1, movie2 ,act2 FROM (
+SELECT actorid as act1, movieid as movie1 FROM (SELECT actorid as id,COUNT(*) as count FROM imdb_movies2actors GROUP BY actorid)A JOIN imdb_movies2actors 
+ON id = actorid  WHERE count >= 10)B JOIN (SELECT actorid as act2, movieid as movie2 FROM(SELECT actorid as id,COUNT(*) as count FROM imdb_movies2actors 
+GROUP BY actorid)c JOIN imdb_movies2actors ON id = actorid  WHERE count >= 10)D ON movie1 = movie2 AND act1 != act2)E GROUP BY act1, act2 HAVING count >= 10)F;
 
 
 #question 8
 
-SELECT '1960' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1959 AND year < 1970
-UNION
-SELECT '1970' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1969 AND year < 1980
-UNION
-SELECT '1980' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1979 AND year < 1990
-UNION
-SELECT '1990' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1989 AND year < 2000
-UNION
-SELECT '2000' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1999 AND year < 2011;
-
+SELECT '1960' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1959 AND year < 1970 UNION SELECT '1970' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1969 AND year < 1980
+UNION SELECT '1980' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1979 AND year < 1990 UNION SELECT '1990' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1989 AND year < 2000
+UNION SELECT '2000' AS Decade, COUNT(*) FROM imdb_movies WHERE year > 1999 AND year < 2011;
 
 
 #question 9
 
-SELECT COUNT(*), movieid, FROM 
-(SELECT movieid, sex FROM imdb_movies2actors JOIN imdb_actors ON imdb_movies2actors.actorid = imdb_actors.actorid WHERE sex = 'F')a
- GROUP BY movieID;
+SELECT COUNT(*) AS Difference FROM (SELECT movieid as id, COUNT(*) as FemalesCount FROM imdb_movies2actors JOIN imdb_actors ON imdb_movies2actors.actorid = imdb_actors.actorid 
+WHERE sex = 'F' GROUP BY movieid)A JOIN (SELECT movieid as id2, COUNT(*) as MalesCount FROM imdb_movies2actors JOIN imdb_actors ON imdb_movies2actors.actorid = imdb_actors.actorid 
+WHERE sex = 'M' GROUP BY movieid)B ON id = id2 WHERE FemalesCount > MalesCount;
 
 
 
 #question 10
 
-SELECT * FROM 
-(
-SELECT genre, AVG(rank) as ranking FROM 
-(
-SELECT imdb_ratings.movieid, rank, votes, genre FROM imdb_ratings JOIN imdb_movies2directors ON imdb_ratings.movieid = imdb_movies2directors.movieid WHERE votes >= 10000
-)a GROUP BY genre
-)b ORDER BY ranking DESC LIMIT 1;
-
+SELECT * FROM (SELECT genre, AVG(rank) as AvgRank FROM (SELECT imdb_ratings.movieid, rank, votes, genre FROM imdb_ratings JOIN imdb_movies2directors 
+ON imdb_ratings.movieid = imdb_movies2directors.movieid WHERE votes >= 10000)A GROUP BY genre)B ORDER BY AvgRank DESC LIMIT 1;
 
 
 #question 11
 
-SELECT id, name, genres FROM
-(
-SELECT actorid as id, COUNT(DISTINCT(genre)) AS genres FROM imdb_movies2directors JOIN imdb_movies2actors ON imdb_movies2directors.movieid = imdb_movies2actors.movieid GROUP BY actorid 
-)a JOIN imdb_actors ON id = actorid  WHERE genres > 9;
-
+SELECT id, name, genres FROM(SELECT actorid as id, COUNT(DISTINCT(genre)) AS genres FROM imdb_movies2directors JOIN imdb_movies2actors 
+ON imdb_movies2directors.movieid = imdb_movies2actors.movieid GROUP BY actorid)A JOIN imdb_actors ON id = actorid  WHERE genres > 9;
 
 
 #question 12
 
-SELECT COUNT(*) FROM
-(
-SELECT id, imdb_directors.name as dirname, imdb_actors.name as actname, imdb_writers.name as wrtname FROM
-(
-SELECT directorid as dir, actorid as act, writerid as wrt, imdb_movies2directors.movieid as id FROM imdb_movies2directors JOIN imdb_movies2writers ON imdb_movies2directors.movieid = imdb_movies2writers.movieid JOIN imdb_movies2actors ON imdb_movies2directors.movieid = imdb_movies2actors.movieid 
-)a 
-JOIN imdb_directors ON directorid = dir 
-JOIN imdb_actors ON actorid = act 
-JOIN imdb_writers ON writerid = wrt
-)b WHERE dirname = actname AND actname = wrtname;
-
+SELECT COUNT(*) AS OverallCount FROM (SELECT id, ActorName, WritersName, DirectorNames FROM(SELECT id, imdb_directors.name AS DirectorNames, imdb_actors.name 
+AS ActorName, imdb_writers.name AS WritersName FROM (SELECT directorid AS Did, actorid AS Aid, writerid AS Wid, imdb_movies2directors.movieid AS id 
+FROM imdb_movies2directors JOIN imdb_movies2writers ON imdb_movies2directors.movieid = imdb_movies2writers.movieid JOIN imdb_movies2actors 
+ON imdb_movies2directors.movieid = imdb_movies2actors.movieid)A JOIN imdb_directors ON directorid = Did JOIN imdb_actors ON actorid = Aid 
+JOIN imdb_writers ON writerid = Wid)B WHERE DirectorNames = ActorName AND ActorName = WritersName GROUP BY id)C;
 
 
 #question 13
- 
-SELECT MAX(ranking), Decade FROM (
-SELECT AVG(rank) AS ranking, '1900' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1900 AND 1909
-UNION
-SELECT AVG(rank) AS ranking, '1910' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1910 AND 1919
-UNION
-SELECT AVG(rank) AS ranking, '1920' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1920 AND 1929
-UNION
-SELECT AVG(rank) AS ranking, '1930' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1930 AND 1939
-UNION
-SELECT AVG(rank) AS ranking, '1940' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1940 AND 1949
-UNION
-SELECT AVG(rank) AS ranking, '1950' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1950 AND 1959
-UNION
-SELECT AVG(rank) AS ranking, '1960' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1960 AND 1969
-UNION
-SELECT AVG(rank) AS ranking, '1970' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1970 AND 1979
-UNION
-SELECT AVG(rank) AS ranking, '1980' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1980 AND 1989
-UNION
-SELECT AVG(rank) AS ranking, '1990' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 1990 AND 1999
-UNION
-SELECT AVG(rank) AS ranking, '2000' as Decade, year FROM imdb_movies JOIN imdb_ratings ON imdb_movies.movieid = imdb_ratings.movieid WHERE year Between 2000 AND 2009)a;
 
+SELECT AvgRank, Decade FROM (SELECT AVG(rank) as AvgRank , ((year DIV 10) * 10) as Decade FROM imdb_movies JOIN imdb_ratings 
+ON imdb_movies.movieid = imdb_ratings.movieid GROUP BY year DIV 10)A ORDER BY AvgRank DESC LIMIT 1;
 
 
 #question 14
 
 SELECT COUNT(*) FROM imdb_movies2directors WHERE genre IS NULL;
 
-
-
 #question 15
 
-SELECT COUNT(*) FROM
-(
-SELECT id, imdb_directors.name as dirname, imdb_actors.name as actname, imdb_writers.name as wrtname, lead FROM
-(
-SELECT directorid as dir, 
-actorid as actor, 
-writerid as writer, 
-imdb_movies2directors.movieid as id, 
-imdb_movies2actors.leading as lead  
-FROM imdb_movies2directors 
-JOIN imdb_movies2writers ON 
-imdb_movies2directors.movieid = imdb_movies2writers.movieid 
-JOIN imdb_movies2actors ON 
-imdb_movies2directors.movieid = imdb_movies2actors.movieid 
-)a 
-JOIN imdb_directors ON directorid = dir 
-JOIN imdb_actors ON actorid = actor 
-JOIN imdb_writers ON writerid = writert
-)b WHERE dirname = actname AND actname = wrtname AND lead > 1;
-
-
-
+SELECT COUNT(*) AS DirCount FROM(SELECT id, ActorName, WritersName, DirectorNames FROM (SELECT id, imdb_directors.name as DirectorNames, imdb_actors.name 
+as ActorName, imdb_writers.name as WritersName, lead FROM( SELECT directorid as Did, actorid as Aid, writerid as Wid, imdb_movies2directors.movieid as id, 
+imdb_movies2actors.leading as lead FROM imdb_movies2directors JOIN imdb_movies2writers ON imdb_movies2directors.movieid = imdb_movies2writers.movieid 
+JOIN imdb_movies2actors ON imdb_movies2directors.movieid = imdb_movies2actors.movieid)A JOIN imdb_directors ON directorid = Did 
+JOIN imdb_actors ON actorid = Aid JOIN imdb_writers ON writerid = Wid)B WHERE DirectorNames = ActorName AND ActorName = WritersName AND lead > 1
+GROUP BY id)C;
 
